7fef8336b54dd77391166ebe7c682c265485179b
Inigo Goiri <inigog@microsoft.com>
2026-03-26 23:05:32 +0000

    Fix

--- tests/test_flux_parallelize.py
+++ tests/test_flux_parallelize.py
@@ -9,14 +9,15 @@ from unittest.mock import MagicMock
 from unittest.mock import patch
 
 from tests.torch_mock import TorchMock
+from tests.diffusers_mock import DiffusersMock
 
 mock_torch = TorchMock()
+mock_diffusers = DiffusersMock()
 
 sys.path.append("wrapper")
 sys.path.append("wrapper/flux")
 
-with patch.dict(sys.modules, {
-    'torch': mock_torch,
+mock_modules =  {
     'xfuser': MagicMock(),
     'xfuser.config': MagicMock(),
     'xfuser.core': MagicMock(),
@@ -26,8 +27,11 @@ with patch.dict(sys.modules, {
     'xfuser.model_executor.models.transformers.transformer_flux': MagicMock(),
     'xfuser.model_executor.layers': MagicMock(),
     'xfuser.model_executor.layers.attention_processor': MagicMock(),
-    'diffusers': MagicMock(),
-}):
+}
+mock_modules.update(mock_torch.get_sub_modules())
+mock_modules.update(mock_diffusers.get_sub_modules())
+
+with patch.dict(sys.modules,):
     from flux.flux_xfuser import parallelize_transformer
 
 
