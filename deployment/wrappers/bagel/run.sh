f4526c9a7a50fa77f74ab1295ddaebfe9fdd1180
Inigo Goiri <inigog@microsoft.com>
2026-04-15 17:55:41 +0000

    PR 285

--- deployment/wrappers/bagel/run.sh
+++ deployment/wrappers/bagel/run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-bash ../run_img.sh \
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DEPLOY_DIR="$SCRIPT_DIR/../.."
+SAMPLES_DIR="$SCRIPT_DIR/../../../benchmark/samples"
+
+bash "$DEPLOY_DIR/run_img.sh" \
   --path bagel \
-  --img ../../benchmark/samples/sample.png \
+  --img "$SAMPLES_DIR/sample.png" \
   "$@"
